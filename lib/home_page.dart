64edddb9da44b2b2d95dd2179f8aedca99611279
anjalipoudel <sabnom77poudel@gmail.com>
2022-12-04 16:28:44 +0545

    init

---
+++ lib/home_page.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:meroapp/model/content_model.dart';
+import 'package:meroapp/network/api_services.dart';
+import 'package:meroapp/screens/details_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+            "Meropadai",
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          actions: const [DrawerWidget()]),
+      drawer: Drawer(
+        child: SafeArea(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Align(
+              alignment: Alignment.centerRight,
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.close,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                "Home",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                "SEE 2079",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                "CSIT Entrance",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ),
+          ]),
+        ),
+      ),
+      body: SafeArea(
+        child: FutureBuilder(
+            future: ApiServices.getContent(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<ContentModel> result = snapshot.data as List<ContentModel>;
+                return Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: ListView.separated(
+                    itemCount: result.length,
+                    separatorBuilder: (context, index) => const SizedBox(
+                      height: 10,
+                    ),
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DetailsPage(
+                                details: result[index].details!,
+                                imageUrl: result[index].imageUrl!,
+                              ),
+                            ),
+                          );
+                        },
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 200,
+                              width: double.infinity,
+                              color: Colors.grey,
+                              child: Image.network(
+                                result[index].imageUrl!,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10),
+                              child: Text(
+                                result[index].title!,
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                child:
+                                    Html(data: result[index].shortDescription!)
+                                // Text(
+                                //   result[index].shortDescription!,
+                                //   style: TextStyle(
+                                //     fontSize: 16,
+                                //   ),
+                                // ),
+                                ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                );
+              } else if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Container(
+                    child: const Text("Err"),
+                  ),
+                );
+              }
+              return Container();
+            }),
+      ),
+    );
+  }
+}
+
+class DrawerWidget extends StatelessWidget {
+  const DrawerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          Scaffold.of(context).openDrawer();
+        },
+        icon: const Icon(
+          Icons.menu,
+          color: Colors.black,
+        ));
+  }
+}
